a0cfd4109c0ecce39f72b97deff6cf2c19a4b00c
DumontALINE <aline.dmnt83@gmail.com>
2026-09-25 09:38:25 +0200

    feat: ajout du front de la page connecxion

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,5 +1,18 @@
-function login() {
-  return <h1>Page de connexion</h1>;
+import "./login.css";
+
+function Login() {
+  return (
+    <main className="conteneur-login">
+      <section className="login-card">
+        <h1>Supervision Passive Recorder</h1>
+        <label htmlFor="token">Token (24 characters)</label>
+        <input id="token" type="password" maxLength={24} placeholder="••••••••••••••••••••••••" />
+
+        <button>Login</button>
+        <a href="#">Obtenir Token</a>
+      </section>
+    </main>
+  );
 }
 
-export default PageLogin;
+export default Login;
